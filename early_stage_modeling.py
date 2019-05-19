303f6613ea6bbddf281eaa1e062943b89fd71d7e
Jared Berry <jareddberry@gmail.com>
2019-05-18 21:27:01 -0400

    Added EMA smoothing, activated merge for all features, partitioned out LGBM modeling

--- early_stage_modeling.py
+++ early_stage_modeling.py
@@ -2,9 +2,11 @@
 """
 Early-stage modeling
 
-05/11/2019
+05/18/2019
 Jared Berry
 """
+
+# SET-UP ----------------------------------------------------------------------
 
 # Import necessary libraries for data preparation/modeling
 import os
@@ -15,79 +17,170 @@ import numpy as np
 
 # Load datasets into memory
 yahoo = pd.read_csv('stock_price_until_2019_04_28.csv')
+drvd = pd.read_csv('moving-avg-momentum.csv')
 simfin = pd.read_csv('simfin\daily_simfin.csv')
 
 # Check dimensions
 print("Yahoo! Finance data has {} observations and {} features.".format(yahoo.shape[0], yahoo.shape[1]))
+print("Derived data has {} observations and {} features.".format(drvd.shape[0], drvd.shape[1]))
 print("Daily SimFin data has {} observations and {} features.".format(simfin.shape[0], simfin.shape[1]))
 
 # Check keys
-print("SimFin:")
-print(simfin[['ticker', 'date']].head())
 print("Yahoo! Finance:")
 print(yahoo[['Symbol', 'date_of_transaction']].head())
+print("Derived (Yahoo! Finance)")
+print(drvd[['Symbol', 'Date']].head())
+print("SimFin:")
+print(simfin[['ticker', 'date']].head())
 
 # Some quick fixes on keys
 yahoo['ticker'] = yahoo['Symbol']
 yahoo.drop('Symbol', axis=1, inplace=True)
 
+drvd['ticker'] = drvd['Symbol']
+drvd['date_of_transaction'] = drvd['Date']
+drvd.drop(['Symbol', 'Date', 'High', 'Low', 
+           'Open', 'Close', 'Volume', 'AdjClose'], 
+          axis=1, inplace=True)
+
 simfin['date_of_transaction'] = simfin['date']
 simfin.drop('date', axis=1, inplace=True)
 
-# SET UP TARGET
-train = yahoo ## pd.merge(yahoo, simfin, on=['ticker', 'date_of_transaction'])
+# MERGE -----------------------------------------------------------------------
+
+train = pd.merge(yahoo, drvd, on=['ticker', 'date_of_transaction'])
+train = pd.merge(train, simfin, how='left', on=['ticker', 'date_of_transaction'])
+
+train = train.sort_values(['ticker','date_of_transaction'])
+train = train[train['date_of_transaction'] >= '2011-03-31'].reset_index().drop('index', axis=1)
 train.head()
 
-# Testing out creation of a target
-train = train.loc[train['ticker'] == 'ABT',:]
-train_21 = train['AdjClose'].shift(-21) # WILL NEED TO DO THIS FOR EACH COMPANY
-## pd.concat([train, train_21], axis=1)
-train_21.name = 'AdjClose_ahead'
-y_cont = np.array(100*((train_21 - train['AdjClose'])/train['AdjClose']))
-y_disc = np.where(y_cont >= 0, 1, 
-         np.where(np.isnan(y_cont), np.nan, 0))
+# FEATURE ENGINEERING (ADDITIONAL) --------------------------------------------
 
-# CHOOSE FEATURES
+train['eps'] = train['net_income_y'] / train['common_outstanding_basic']
+train['pe_ratio'] = train['AdjClose'] / train['eps']
+train['debt_ratio'] = train['total_liabilities'] / train['total_equity']
+train['debt_to_equity'] = train['total_liabilities'] / train['total_equity']
+train['roa'] = train['net_income_y'] / train['total_assets']
+ 
+train = train.loc[train['ticker'] == 'A',:]
+
+# SET UP TARGET ---------------------------------------------------------------
+
+# Computing all of the returns for the next 21 days (month) relative to today
+aheads = []
+for i in range(0,22):
+    aheads.append(np.array(100*((train['AdjClose'].shift(-i) - train['AdjClose'])/train['AdjClose'])))
+
+# Average of those returns
+target = np.array(pd.DataFrame(aheads).mean(axis=0, skipna=False).tolist())
+
+# CHOOSE FEATURES -------------------------------------------------------------
 features = ['High', 'Low', 'Open', 'Close', 'Volume', 'AdjClose', 'Year',
-            'Month', 'Week', 'Day', 'Dayofweek', 'Dayofyear']
+            'Month', 'Week', 'Day', 'Dayofweek', 'Dayofyear', 'Pct_Change_Daily',
+            'Pct_Change_Monthly', 'Pct_Change_Yearly', 'RSI', 'Volatility',
+            'Yearly_Return_Rank', 'Monthly_Return_Rank', 'Pct_Change_Class',
+            'Rolling_Yearly_Mean_Positive_Days', 'Rolling_Monthly_Mean_Positive_Days', 
+            'Rolling_Monthly_Mean_Price', 'Rolling_Yearly_Mean_Price',
+            'pe_ratio', 'debt_ratio', 'debt_to_equity', 'roa'
+            #'total_assets', 'total_equity', 'total_liabilities', 'net_income_y',
+            #'operating_expenses', 'gross_profit', 'retained_earnings', 'revenue'
+            ]
 
 X = train[features]
-X[['High_diff', 'Low_diff', 'Open_diff', 'Close', 'Volume', 'AdjClose']] =        \
-X[['High', 'Low', 'Open', 'Close', 'Volume', 'AdjClose']]  -       \
-X[['High', 'Low', 'Open', 'Close', 'Volume', 'AdjClose']].shift(1)
-X = X.apply(lambda x: (x - np.mean(x))/np.std(x)).fillna(0) # Impute 0 for now
+X = X.apply(lambda x: (x - np.mean(x))/np.std(x)).fillna(0)
 
-# Split out mechanically-missing data
-test_idx = np.where(np.isnan(y_disc))[0].tolist()
+# PARTITIONING ----------------------------------------------------------------
 
-y = np.delete(y_disc, test_idx)
+# Hold out mechanically-missing data
+test_idx = np.where(np.isnan(target))[0].tolist()
+
+y = np.delete(target, test_idx)
 X_holdout = X.loc[X.index[test_idx]]
 X = X.drop(X.index[test_idx])
+   
+# LightGBM --------------------------------------------------------------------
 
-# SPLIT
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=101)
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import TimeSeriesSplit
+import lightgbm as lgb
 
-# MODEL
-from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier()
+# Convert to NumPy arrays - store feature names
+feature_names = X.columns.tolist()
+features = np.array(X)
+labels = y.copy()
 
-tscv = TimeSeriesSplit(n_splits=12).split(X)
-gsearch = GridSearchCV(estimator=model, cv=tscv,
-                       param_grid=param_search)
+EMA = 0
+gamma = 0.05
+for ti in range(len(labels)):
+    EMA = gamma*labels[ti] + (1-gamma)*EMA
+    labels[ti] = EMA   
 
-knn.fit(X_train, y_train)
-y_pred = knn.predict(X_test)
+pd.DataFrame(labels).plot()
 
-print(confusion_matrix(y_test, y_pred))
-print(classification_report(y_test, y_pred))
+# Generate splits
+splits = TimeSeriesSplit(n_splits=12)
 
-from sklearn.ensemble import GradientBoostingClassifier
-gbc = GradientBoostingClassifier()
+# Empty array for feature importances
+feature_importance_values = np.zeros(len(feature_names))
+    
+# Empty array for out of fold validation predictions
+out_of_fold = np.zeros(features.shape[0])
+    
+# Lists for recording validation and training scores
+valid_scores = []
+train_scores = []
 
-gbc.fit(X_train, y_train)
-y_pred = gbc.predict(X_test)
+# Iterate through each fold
+for train_indices, valid_indices in splits.split(X): 
+        
+    # Training data for the fold
+    train_features, train_labels = features[train_indices], labels[train_indices]
+    #Validation data for the fold
+    valid_features, valid_labels = features[valid_indices], labels[valid_indices]
+    
+    EMA = 0
+    gamma = 1
+    for ti in range(len(train_labels)):
+        EMA = gamma*train_labels[ti] + (1-gamma)*EMA
+        train_labels[ti] = EMA 
+        
+    train_labels = np.where(train_labels >= 0, 1, 0)
+    valid_labels = np.where(valid_labels >= 0, 1, 0)
+    
+    # Create the bst
+    bst = lgb.LGBMClassifier(n_estimators=10000, objective = 'binary', 
+                            class_weight = 'balanced', learning_rate = 0.01,
+                            ## max_bin = 250, num_leaves = 1000,
+                            reg_alpha = 0.1, reg_lambda = 0.1, 
+                            subsample = 0.8, random_state = 101)
+        
+    # Train the bst
+    bst.fit(train_features, train_labels, eval_metric = 'auc',
+            eval_set = [(valid_features, valid_labels), (train_features, train_labels)],
+            eval_names = ['valid', 'train'],
+            early_stopping_rounds = 100, verbose = 200)
+    
+    # Record the best iteration
+    best_iteration = bst.best_iteration_
+        
+    # Record the feature importances
+    feature_importance_values += bst.feature_importances_ / splits.n_splits
 
-print(confusion_matrix(y_test, y_pred))
-print(classification_report(y_test, y_pred))
+    # Record the out of fold predictions
+    out_of_fold[valid_indices] = bst.predict_proba(valid_features, num_iteration = best_iteration)[:, 1]
+        
+    # Record the best score
+    print(bst.best_score_['valid'])
+    valid_score = bst.best_score_['valid']['auc']
+    train_score = bst.best_score_['train']['auc']
+        
+    valid_scores.append(valid_score)
+    train_scores.append(train_score) 
+  
+print("\n")
+print(valid_scores)
+print("\n")
+print(np.mean(valid_scores))
+print("\n")
+print(sorted(list(zip(feature_names, feature_importance_values)), key=lambda x: x[1], reverse=True))
+     
