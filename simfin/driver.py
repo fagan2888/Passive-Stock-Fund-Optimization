213f007a94244829a573a32290eb1522d699396f
Jared Berry <jareddberry@gmail.com>
2019-06-06 20:03:38 -0400

    Fixed driver variables

--- simfin/driver.py
+++ simfin/driver.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Set up driver to avoid requisite passing of API key directly through user 
+Set up driver to avoid requisite passing of API key directly through user
 input. All parameters should be set manually below, and are passed into
 machinery which hits the SimFin API
 
@@ -8,8 +8,9 @@ machinery which hits the SimFin API
 Jared Berry
 """
 
-key = '' # Enter API key here on local copy
-quarters = ['Q1','Q2','Q3','Q4']
-years = ['2011','2012','2013','2014','2015','2016','2017','2018','2019']
-statement_types = ['pl','bs','cf']
-use_process_file = True
+KEY = ''
+QUARTERS = ['Q1', 'Q2', 'Q3', 'Q4']
+YEARS = ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019']
+STATEMENT_TYPES = ['pl', 'bs', 'cf']
+PULL_SQL = False
+USE_PROCESS_FILE = True
